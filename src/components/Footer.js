dd658a216401c76967ef352aaf6b273700878476
Ayokunle Odedere <Ayokunleodedere@panoramicsynergy.co.uk>
2020-08-25 00:24:59 +0100

    changed logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
   <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} CovenantOlodo.com</p>
+    <p>&copy; {new Date().getFullYear()} Ayokunle.org</p>
   </footer>
   );
 }
