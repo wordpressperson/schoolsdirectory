43eee47bbbd7f6d92279d1e6c742ec3d767e1a9d
Ayokunle Odedere <Ayokunleodedere@panoramicsynergy.co.uk>
2020-08-25 01:04:46 +0100

    changed stuffz

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
   <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} Ayokunle.org</p>
+    <p>&copy; {new Date().getFullYear()} Ayokunle.xyz</p>
   </footer>
   );
 }
